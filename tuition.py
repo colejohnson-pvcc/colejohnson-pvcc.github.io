925b29330089e3dcd2f973a0b7fe2810126fdb25
colejohnson-pvcc <157636220+colejohnson-pvcc@users.noreply.github.com>
2024-03-26 13:27:00 -0400

    tuition.py

--- tuition.py
+++ tuition.py
@@ -15,6 +15,13 @@ inout = 1  # 1 means in-state, 2 means out-of-state
 numcredits = 0
 scholarshipamt = 0
 
+tuition_amt = 0
+inst_fee = 0
+cap_fee = 0
+act_fee = 0
+total = 0
+balance = 0
+
 
 ##############  define program functions ##############
 def main():
@@ -32,47 +39,44 @@ def main():
 
 def get_user_data():
     global inout, numcredits, scholarshipamt
+    print('**** PIEDMONT VIRGINIA COMM COLLEGE Tuition & Fees ****')
     inout = int(input("Enter a 1 for IN-STATE; enter a 2 for OUT-OF-STATE: "))
     numcredits = int(input("Number of credits registered for: "))
     scholarshipamt = float(input("Scholarship amount received: "))
 
 def perform_calculations():
-    global in_state_tuition, out_of_state_tuition, capital_fee, institution_fee, activity_fee, total, scholarship_amount, balance
-    in_state_tuition = numcredits * RATE_TUITION_IN
-    out_of_state_tuition = numcredits * RATE_TUITION_OUT
-    capital_fee = numcredits * RATE_CAPITAL_FEE
-    institution_fee = numcredits * RATE_INSTITUTION_FEE
-    activity_fee = numcredits * RATE_ACTIVITY_FEE
+    global tuition, cap_fee, inst_fee, act_fee, total, balance
     
     if inout == 1:
-        total = numcredits * RATE_TUITION_IN + institution_fee + activity_fee
-        balance = total - scholarshipamt
+        tuition = numcredits * RATE_TUITION_IN
+        cap_fee = 0
     else:
-        total = numcredits * RATE_TUITION_OUT + capital_fee + institution_fee + activity_fee
-        balance = total - scholarshipamt
-    
+        tuition = numcredits * RATE_TUITION_OUT
+        cap_fee = numcredits * RATE_CAPITAL_FEE
 
+    inst_fee = RATE_INSTITUTION_FEE
+    inst_fee = numcredits * RATE_INSTITUTION_FEE
+    act_fee = numcredits * RATE_ACTIVITY_FEE
+    total = tuition + cap_fee + inst_fee + act_fee
+    balance = total - scholarshipamt
+    
 def display_results():
-    if inout == 1:
-        print('In-State Tuiton         $ ' + format(in_state_tuition,'8,.2f'))
-        print('Institution Fee         $ ' + format(institution_fee,'8,.2f'))
-        print('Activity Fee            $ ' + format(activity_fee,'8,.2f'))
-        print('Total                   $ ' + format(total,'8,.2f'))
-        print('Scholarship             $ ' + format(scholarshipamt,'8,.2f'))
-        print('Balance                 $ ' + format(balance,'8,.2f'))
-        print('------------------------------')
-        print(str(datetime.datetime.now()))
-    else:
-        print('Out-of-State Tuition    $ ' + format(out_of_state_tuition,'8,.2f'))
-        print('Capital Fee             $ ' + format(capital_fee,'8,.2f'))
-        print('Institution Fee         $ ' + format(institution_fee,'8,.2f'))
-        print('Activity Fee            $ ' + format(activity_fee,'8,.2f'))
-        print('Total                   $ ' + format(total,'8,.2f'))
-        print('Scholarship             $ ' + format(scholarshipamt,'8,.2f'))
-        print('Balance                 $ ' + format(balance,'8,.2f'))
-        print('------------------------------')
-        print(str(datetime.datetime.now()))
+    currency = '8,.2f'
+    line = '-----------------------------------------'
+
+    print(line)
+    print('**** PIEDMONT VIRGINIA COMM COLLEGE ****')
+    print('    Tuition & Fees Report')
+    print(str(datetime.datetime.now()))
+    print(line)
+    print('Tuiton           $ ' + format(tuition, currency))
+    print('Capital Fee      $ ' + format(cap_fee, currency))
+    print('Institution Fee  $ ' + format(inst_fee, currency))
+    print('Activity Fee     $ ' + format(act_fee, currency))
+    print('Total            $ ' + format(total, currency))
+    print('Scholarship      $ ' + format(scholarshipamt, currency))
+    print('Balance          $ ' + format(balance, currency))
+    print('------------------------------')
 
 ##########  call on main program to execute ##########
 main()
-
